c4622ef7bba1ad7709e487910da7b389b759a5d9
shubham.goel <shubham.goel@LP2-AP-51833546.PROD.HCLPNP.COM>
2020-01-12 16:18:22 +0530

    http post

--- src/main/java/com/spring/brewery/web/controller/BeerController.java
+++ src/main/java/com/spring/brewery/web/controller/BeerController.java
@@ -2,10 +2,12 @@ package com.spring.brewery.web.controller;
 
 import java.util.UUID;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +29,17 @@ public class BeerController {
 
 		return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
 
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> handlePost(BeerDto beerDto) {
+		BeerDto savedDto = beerService.saveNewBeer(beerDto);
+		
+		HttpHeaders headers = new HttpHeaders();
+		//ToDo add hostname to url
+		headers.add("Location", "/api/v1/beer"+savedDto.getId().toString());
+		return new ResponseEntity<>(headers, HttpStatus.CREATED);
+		
 	}
 
 }
--- src/main/java/com/spring/brewery/web/service/BeerServiceImpl.java
+++ src/main/java/com/spring/brewery/web/service/BeerServiceImpl.java
@@ -17,4 +17,11 @@ public class BeerServiceImpl implements BeerService{
 				.build();
 	}
 
+	@Override
+	public BeerDto saveNewBeer(BeerDto beerDto) {
+		
+		return BeerDto.builder().id(UUID.randomUUID())
+				.build();
+	}
+
 }
